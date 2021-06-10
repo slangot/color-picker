9a5d65e390b04b345a7603d1d2e39c487c7331ee
Shawn <shawn17@live.fr>
2021-06-10 15:06:18 +0200

    Ajout boutons et input de copie, script de copie fonctinnel

--- script.js
+++ script.js
@@ -1,35 +1,50 @@
 /****** VARIABLES */
 
+// Text Color
 const textColor = document.querySelector('#text-color');
 const textColorResult = document.querySelector('#result-text');
 
+// BG Color
 const bgColor = document.querySelector('#bg-color');
 const bgColorResult = document.querySelector('#result-container');
 
-const textColorCode = document.querySelector('#text-color-code');
-const bgColorCode = document.querySelector('#bg-color-code');
+// Color Code
+const textColorCode = document.querySelector('#copy-text-color-code');
+const bgColorCode = document.querySelector('#copy-bg-color-code');
 
+// Copy Buttons
 const copyTextColor = document.querySelector('#copy-text-color');
 const copyBGColor = document.querySelector('#copy-bg-color');
 
 
 /****** EVENTS */
 
-bgColor.addEventListener('change', () => {
-    bgColorResult.style.backgroundColor = bgColor.value;
-    bgColorCode.value = bgColor.value;
-    console.log(bgColorCode.value);
-});
+
+// Text Color Events
 
 textColor.addEventListener('change', () => {
     textColorResult.style.color = textColor.value;
     textColorCode.value = textColor.value;
+    textColorCode.textContent = textColor.value;
     console.log(textColorCode.value);
 });
 
 copyTextColor.addEventListener('click', () => {
-    console.log(textColorCode.value, textColorCode.select());
     textColorCode.select();
     document.execCommand('copy');
+});
 
+
+// BG Color Events
+
+bgColor.addEventListener('change', () => {
+    bgColorResult.style.backgroundColor = bgColor.value;
+    bgColorCode.value = bgColor.value;
+    bgColorCode.textContent = bgColor.value;
+    console.log(bgColorCode.value);
+});
+
+copyBGColor.addEventListener('click', () => {
+    bgColorCode.select();
+    document.execCommand('copy');
 });
